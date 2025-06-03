bfd93812e1283f0a7e96cc5eb530a295a8938736
jkatahre <jitendrakatahre@gmail.com>
2025-06-03 08:32:38 +0530

    add drag & drop feature

--- script/script.js
+++ script/script.js
@@ -4,7 +4,8 @@ document.addEventListener("DOMContentLoaded", function () {
     let item = document.querySelector("#input");
     let To_Do = document.querySelector("#li");
     let complite_Work = document.querySelector("#li2");
-    let running_work = document.querySelector("#running")
+    // let running_work = document.querySelector("#running")
+    let running_work = document.querySelector(".running_container")
     let pending_work = document.querySelector("#pending")
 
     let itemsList = [] 
@@ -40,10 +41,11 @@ document.addEventListener("DOMContentLoaded", function () {
     item.addEventListener("keyup", (e) => {
         //document.querySelector(".store").innerHTML = "<div> Radhe Radhe </div>"
         // console.log(e.key)
-        if (e.key == "Enter") {
+        if (e.key == "Enter" && item.value != "") {
             addItem(item.value)
             item.value = "";
         }
+
     })
     let addItem = (item) => {
 
@@ -54,7 +56,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         //Object key 
         let currTask = {
-            id: Math.random(1, 5000),
+            id: parseInt(Math.random() * 100 + 1),
             taskName: item,
             date: "",
             isCompleted: false,
@@ -80,8 +82,12 @@ document.addEventListener("DOMContentLoaded", function () {
         let time = date.toLocaleString()
         // for (let i=0; i < itemsList.length; i++) {
         // let currItem = itemsList[i]
-        let new_item = document.createElement("li");
-        new_item.innerHTML = `${currItem.taskName} <p class="time" >${time}</p> <button class="btn2"><img style="height: 22px; margin: 1px 5px;" src="assets/png/yes.png" alt="yes"></button><button class="btn"><img class="img" src="assets/png/cancel.png" alt="cancle"></button>`
+        // let new_item = document.createElement("li");
+        let new_item = document.createElement("div");
+        new_item.className = "newItems"
+        new_item.setAttribute('draggabke', 'true');
+        // new_item.innerHTML = `${currItem.taskName} <p class="time" >${time}</p> <button class="btn2"><img style="height: 22px; margin: 1px 5px;" src="assets/png/yes.png" alt="yes"></button><button class="btn"><img class="img" src="assets/png/cancel.png" alt="cancle"></button>`
+        new_item.innerHTML = `<div class="task_container" draggable="true">${currItem.taskName} <p class="time" >${time}</p> <button class="btn2"><img style="height: 22px; margin: 1px 5px;" src="assets/png/yes.png" alt="yes"></button><button class="btn"><img class="img" src="assets/png/cancel.png" alt="cancle"></button></div>`
         // To_Do.appendChild(new_item)
         running_work.appendChild(new_item)
 
@@ -119,4 +125,47 @@ document.addEventListener("DOMContentLoaded", function () {
         localStorage.setItem("task", JSON.stringify(itemsList))
         // }
     }
+
+    let runningItem = document.querySelector(".running_container");
+    let pandingItem = document.querySelector(".panding_container");
+    let completeItem = document.querySelector(".complete_container");
+
+
+    let newItems = document.querySelectorAll(".newItems")
+    for(lists of newItems){
+        lists.addEventListener("dragstart", (e)=>{
+            let select = e.target
+            console.log(select)
+            runningItem.addEventListener("dragover", (e)=>{
+                e.preventDefault();
+
+            })
+            runningItem.addEventListener("drop", (e)=>{
+                runningItem.appendChild(select)
+                select = "";
+            })
+
+            pandingItem.addEventListener("dragover", (e)=>{
+                e.preventDefault();
+            })
+            pandingItem.addEventListener("drop", (e)=>{
+                pandingItem.appendChild(select)
+                select="";
+               
+            })
+            completeItem.addEventListener("dragover", (e)=>{
+                e.preventDefault();
+
+            })
+            completeItem.addEventListener("drop", (e)=>{
+                completeItem.appendChild(select)
+                select = "";
+            })
+          
+
+        })
+
+    }
+
+
 })
